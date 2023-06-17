deca115b0ec63bcb8b2c38edfd2d1987ca646f4f
Harsh8279 <hhenu8279@gmail.com>
2023-06-17 10:49:09 +0530

    playing with print statement

---
+++ prg_1.py
@@ -0,0 +1,8 @@
+# print statement
+
+print("Hello World")
+print('Hello World')
+print('Hello World!\nThis is Harsh Patel')
+print('Hello World!\tThis is Harsh Patel')
+print("Let's play the game")
+print('Hello This is "Harsh" Patel')
